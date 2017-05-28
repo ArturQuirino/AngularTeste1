7bfc3e119a5b18c2b329fbd7dccf8f49052195d2
ArturQuirino <arturquirino6@gmail.com>
2017-05-28 17:48:15 -0300

    tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  {
+  private currentUser:User = {username: "Test", email:"test@test.com"}
+
+  private hideEmail:boolean = true
+
+  private toggle(){
+    this.hideEmail = !this.hideEmail
+  }
+
+  private changeMail(newMail:string){
+    this.currentUser.email = newMail
+  }
+
+  private keyPress(event:KeyboardEvent){
+    if(event.keyCode == 13){
+      var inputElement = <HTMLInputElement>event.target
+      this.changeMail(inputElement.value)
+      
+    }
+  }
+}
+
+interface User{
+  username:string,
+  email:string
+}
