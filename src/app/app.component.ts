9ffdf16e95c2c0725dd3bc41ac02e594daa59d59
ArturQuirino <arturquirino6@gmail.com>
2017-05-28 17:01:42 -0300

    Tutorial angular parte 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html',
-  styles:['.blue-bg{background-color:blue;}']
+  templateUrl: 'app/app.component.html'
 })
 
 export class AppComponent  {
   private currentUser:User = {username: "Test", email:"test@test.com"}
 
-  private hideEmail:boolean = false
+  private hideEmail:boolean = true
 
-  private disabledInput:boolean = false
-  private isDisabled():boolean{
-    return this.disabledInput
+  private toggle(){
+    this.hideEmail = !this.hideEmail
   }
-
-  private isBlueBg:boolean = true
-  private bgColor:string = 'red'
 }
 
 interface User{
