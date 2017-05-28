ea251713e76d6a25be28784789013807e80d859c
ArturQuirino <arturquirino6@gmail.com>
2017-05-28 14:37:06 -0300

    Alteração par tutorial 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  styles:['.blue-bg{background-color:blue;}']
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  {
+  private currentUser:User = {username: "Test", email:"test@test.com"}
+
+  private hideEmail:boolean = false
+
+  private disabledInput:boolean = false
+  private isDisabled():boolean{
+    return this.disabledInput
+  }
+
+  private isBlueBg:boolean = true
+  private bgColor:string = 'red'
+}
+
+interface User{
+  username:string,
+  email:string
+}
