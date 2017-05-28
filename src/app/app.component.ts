eb68d942555fdc14523c0a6ad5457ac4305e8658
ArturQuirino <arturquirino6@gmail.com>
2017-05-28 17:12:25 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  template: `<h1>Hello {{name}}</h1>`,
 })
-
-export class AppComponent  {
-  private currentUser:User = {username: "Test", email:"test@test.com"}
-
-  private hideEmail:boolean = true
-
-  private toggle(){
-    this.hideEmail = !this.hideEmail
-  }
-}
-
-interface User{
-  username:string,
-  email:string
-}
+export class AppComponent  { name = 'Angular'; }
