d5fa2de8ba260890c921656ba8e669a8124b688d
ArturQuirino <arturquirino6@gmail.com>
2017-10-08 12:27:13 -0300

    Adição de routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,13 @@
-import {Component, ViewChild, Injector} from "@angular/core";
-import {Account} from "./account/account.model";
-import {AccountsList} from "./account/accounts_list.component";
-import {AccountForm} from "./account/account_form.component";
-import {AccountService, ACCOUNT_SERVICE_PROVIDERS} from "./account/account.services"
+import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: 'app/app.component.html',
-  styleUrls:['app/app.component.css'],
-  providers: [ACCOUNT_SERVICE_PROVIDERS],
-
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css'],
+    directives: [ROUTER_DIRECTIVES]
 })
 
-export class AppComponent{
-  private _accounts:Array<Account> = [];
-  private _accountService:AccountService;
-
-  constructor(accountService:AccountService){
-    this._accountService = accountService;
-    var promise = this._accountService.getAll(); 
-    promise.then(accounts => this._accounts = accounts);
-  }
-
-  private createAccError:string = "";
-
-  private createAcc(newAccount:Account){
-    this._accountService.create(newAccount)
-    .then(account => {
-      console.log(account);
-      this.createAccError = "";
-      this.form.resetForm();
-    })
-    .catch(err => this.createAccError = err);
-  }
-
-  private removeAcc(index:number){
-    this._accountService.remove(index).then(account=>console.log(account));
-  }
-
-  @ViewChild(AccountForm) form:AccountForm;
+export class AppComponent {
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
 import { AccountsList } from './account/accounts_list.component';
 import { AccountForm } from './account/account_form.component';
-// import { AccountService } from './account/account.services';
+import { APP_ROUTER_PROVIDERS } from './app.routes';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent , AccountsList, AccountForm],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule,
+                  RouterModule.forRoot(APP_ROUTER_PROVIDERS)],
+
+  declarations: [ AppComponent,
+                  AccountsList,
+                  AccountForm,
+                  HomeComponent,
+                  AccountComponent],
+
+  bootstrap:    [ AppComponent ],
+
+  exports: [RouterModule]
 })
 export class AppModule { }
